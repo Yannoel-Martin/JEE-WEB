e0ba26ebdb9f4a45a72812a7e701823a742dfbfb
Alexis <alexis.pannetier@hotmail.fr>
2019-05-02 17:00:28 +0200

    Servlet update

--- src/servlets/Forum.java
+++ src/servlets/Forum.java
@@ -3,6 +3,7 @@ package servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,6 +16,7 @@ public class Forum extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
+		//TODO: get data from DB
 		String topics = req.getParameter("topics");
 		this.getServletContext().getRequestDispatcher("/WEB-INF/forum.jsp").forward(req, res);
 	}
--- src/servlets/Topic.java
+++ src/servlets/Topic.java
@@ -1,11 +1,16 @@
 package servlets;
 
 import java.io.IOException;
+import java.time.ZonedDateTime;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import beans.Discussion;
+import beans.DiscussionStatus;
 
 public class Topic extends HttpServlet {
 
@@ -13,7 +18,15 @@ public class Topic extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
+		//TODO: get data from DB
 		String discussions = req.getParameter("discussions");
+		
+		Long discussionId = 1L;
+		String discussionName = req.getParameter("discussionName");
+		DiscussionStatus discussionStatus = (DiscussionStatus.Opened);
+		Discussion discussion = new Discussion(discussionId,discussionName,discussionStatus);
+		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/topic.jsp").forward(req, res);
 	}
+	
 }
