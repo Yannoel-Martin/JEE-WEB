442730e239ca7e70e5cc9027f986caa23a475e49
Yannoël <yannoel.martin@hotmail.fr>
2019-05-03 15:44:55 +0200

    validation content

---
+++ src/servlets/ValidationServlet.java
@@ -0,0 +1,98 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Discussion;
+import beans.Message;
+import beans.User;
+import dao.DiscussionDao;
+import dao.GeneralDiscussionDao;
+import dao.GeneralMessageDao;
+import dao.MessageDao;
+import exceptions.NotFoundException;
+
+public final class ValidationServlet extends BaseServlet {
+
+    /** Serial number. */
+	private static final long serialVersionUID = 5622938890054157165L;
+
+	/** Servlet view. */
+	private static final String VIEW = "/WEB-INF/validation.jsp";
+
+	/** Key to access informations on {@link Discussion} in view. */
+	private static final String DISCUSSION_INFO = "discussion";
+
+	/** Key to access linked {@link Message messages} in view. */
+	private static final String MESSAGES_LIST = "messages";
+
+	/** DAO to manipulate {@link Discussion} entities. */
+	private final DiscussionDao discussionDao = new GeneralDiscussionDao();
+
+	/** DAO to manipulate {@link Message} entities. */
+	private final MessageDao messageDao = new GeneralMessageDao();
+
+	@Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse res) throws ServletException, IOException {
+
+        final Long id = this.getPathId(req);
+
+        if (id != null) {
+            try {
+                final Discussion discussion = this.discussionDao.findById(id);
+
+                this.loadView(req, res, discussion);
+            } catch (final NotFoundException e) {
+
+            }
+        }
+	}
+
+	@Override
+	protected void doPost(final HttpServletRequest req, final HttpServletResponse res)
+	        throws ServletException, IOException {
+
+        final Long id = this.getPathId(req);
+
+        if (id != null) {
+            try {
+                final String body = req.getParameter("messageBody");
+                final User user = this.getAuthentificatedUser(req, res);
+                final Discussion discussion = this.discussionDao.findById(id);
+
+                // Send the message.
+                this.messageDao.sendMessage(body, discussion, user);
+
+                this.loadView(req, res, discussion);
+            } catch (final NotFoundException e) {
+
+            }
+        }
+	}
+
+	/**
+	 * Loads the view.
+	 *
+	 * @param req
+	 * @param res
+	 * @param discussion
+	 */
+	private void loadView(final HttpServletRequest req, final HttpServletResponse res, final Discussion discussion) {
+        final User user = this.getAuthentificatedUser(req, res);
+		req.setAttribute("user", user);
+		req.setAttribute(ValidationServlet.DISCUSSION_INFO, discussion);
+
+		final List<Message> messages = this.messageDao.findAll(discussion);
+        req.setAttribute(ValidationServlet.MESSAGES_LIST, messages);
+
+        try {
+            this.getServletContext().getRequestDispatcher(ValidationServlet.VIEW).forward(req, res);
+        } catch (ServletException | IOException e) {
+
+        }
+	}
+}
